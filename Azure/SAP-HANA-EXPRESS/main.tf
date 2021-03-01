3193e2925f847c505061a5ffbe7f4892c6709f6e
Denys Slyvka <32386916+DenysSlyvka@users.noreply.github.com>
2021-03-01 10:52:27 +0100

    Start Terraform Main

---
+++ Azure/SAP-HANA-EXPRESS/main.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x.
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "Flexso-Stage-TestEnv"
+  location = "francecentral"
+}
+
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
--- sap-hana-express/README.md
+++
@@ -1,2 +0,0 @@
-# Flexso_SAP_Autodeploy
-Automatic Depoyment of SAP templates on Azure and Google Cloud
