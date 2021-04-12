1d16d629893c8bf95c45db212f52157c58d549ea
Wernerdeschryver <37301929+Wernerdeschryver@users.noreply.github.com>
2021-04-12 10:12:47 +0200

    Delete GCP/SAP-S4HANA/terraform directory

--- GCP/SAP-S4HANA/terraform/launch.ps1
+++
@@ -1,5 +0,0 @@
-﻿$selection = Read-Host "Please type Confirm"
-cd C:\Users\Dirk\Documents\GitHub\Flexso_SAP_Autodeploy\Azure\SAP-HANA-EXPRESS\terraform
-terraform init
-terraform plan -out sap-hana-express-terraformplan.tfplan
-terraform apply sap-hana-express-terraformplan.tfplan
