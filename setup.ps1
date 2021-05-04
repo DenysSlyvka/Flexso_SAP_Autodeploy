b881e649361d908f8428e8564de5686216673bc7
unknown <denys.slyvka@student.odisee.be>
2021-05-04 15:02:48 +0200

    Aanpassingen uitgevoerd op ABAP en EXPRESS

--- setup.ps1
+++ setup.ps1
@@ -20,6 +20,7 @@ function Show-Menu-Setup-Azure
     Write-Host "================ $Title ================"
     Write-Host "1: SAP HANA Database + NetWeaver ABAP"
     Write-Host "2: SAP HANA Express (test)"
+    Write-Host "3: SAP HANA Database + ABAP Foundation"
     Write-Host "Q: Press 'Q' to quit."
 }
 
@@ -43,6 +44,20 @@ Azure -> SAP-HANA-NETWEAVER-ABAP
 GCP -> SAP-HANA-NETWEAVER-ABAP
 GCP -> SAP-HANA-ABAP-FOUNDATION
 #>
+
+function Deploy-HANA-EXPRESS
+{
+    param (
+        [string]$Path = ''
+    )
+
+    $resourcegroupname = Read-Host -Prompt 'Enter resource group name [default: Azure_SAP_Automatic_Install]'
+
+    cd $Path
+    terraform init
+    terraform plan -out sap-terraformplan.tfplan -var="resourcegroupname=$resourcegroupname"
+    terraform apply sap-terraformplan.tfplan
+}
 function Input-Params-HDB
 {
     param (
@@ -70,7 +85,38 @@ function Input-Params-HDB
 
     cd $path
     terraform init
-    terraform plan -out sap-terraformplan.tfplan -var="hostname=$hostname","resourcegroupname=$resourcegroupname"
+    terraform plan -out sap-terraformplan.tfplan -var="hostname=$hostname" -var="resourcegroupname=$resourcegroupname"
+    terraform apply sap-terraformplan.tfplan
+}
+
+function Input-Params-JAVA
+{
+    param (
+        [string]$Path = ''
+    )
+    $exampleFile = @"
+#this file contains input variables for hana
+---
+"@
+    $exampleFile | Out-File -Encoding utf8 $path\external_vars.yml # C:\Users\Dirk\Documents\GitHub\Flexso_SAP_Autodeploy\GCP
+    $resourcegroupname = Read-Host -Prompt 'Enter resource group name [default: Azure_SAP_Automatic_Install]'
+    $hostname = Read-Host -Prompt 'Enter hostname of the virtual machine [default: myvm]'
+    $sid = Read-Host -Prompt 'Enter SID for HDB server'
+    $instancenr = Read-Host -Prompt 'Enter instance number for HDB server'
+    $nwsid = Read-Host -Prompt 'Enter the SID for NetWeaver [Must be different from the SID for HDB]'
+    $ascsinstancenr = Read-Host -Prompt 'Enter the ASCS instance number for Netweaver [default: 01]'
+    $primaryinstancenr = Read-Host -Prompt 'Enter the instance number for the primary application server [default: 00]'
+
+    Add-Content $Path\external_vars.yml "hostname: $hostname"
+    Add-Content $Path\external_vars.yml "sid: $sid"
+    Add-Content $Path\external_vars.yml "instancenr: '$instancenr'"
+    Add-Content $Path\external_vars.yml "nwsid: $nwsid"
+    Add-Content $Path\external_vars.yml "ascsinstancenr: '$ascsinstancenr'"
+    Add-Content $Path\external_vars.yml "primaryinstancenr: '$primaryinstancenr'"
+
+    cd $path
+    terraform init
+    terraform plan -out sap-terraformplan.tfplan -var="hostname=$hostname" -var="resourcegroupname=$resourcegroupname"
     terraform apply sap-terraformplan.tfplan
 }
 
@@ -85,12 +131,21 @@ Show-Menu-Platform –Title 'Choose platform'
          {
             '1' {
 
-                $path = '..\Flexso_SAP_Autodeploy\Azure\AzureSAPDeploy\terraform'
+                $path = '..\Flexso_SAP_Autodeploy\Azure\SAP-HANA-NETWEAVER-ABAP\terraform'
                 Input-Params-HDB -Path $path
 
             } '2' {
-                echo "Hana express on Azure example"
-            }
+                $path = '..\Flexso_SAP_Autodeploy\Azure\SAP-HANA-EXPRESS\terraform'
+                Deploy-HANA-EXPRESS -Path $path
+
+            } '3' {
+                
+                $path = '..\Flexso_SAP_Autodeploy\Azure\SAP-ASE-JAVA\terraform'
+                Input-Params-HDB -Path $path
+              
+             } 'q' {
+                 return
+             } 
          }
     } '2' { #This contains a switch statement for every option available on the platform 'Google Cloud'
          Show-Menu-Setup-GCP –Title 'Choose setup for Google Cloud'
